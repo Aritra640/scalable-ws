a226a1b334b37b7079952c80ee27863782cc1795
Aritra640 <chatterjeearitra15@gmail.com>
2025-07-16 22:37:08 +0530

    aded second ws client

---
+++ user2-ws/index.ts
@@ -0,0 +1,47 @@
+import { WebSocketServer, WebSocket as WS } from "ws";
+
+const wss = new WebSocketServer({ port: 8082 });
+
+type Room = {
+  sockets: WS[];
+};
+
+const rooms: Record<string, Room> = {};
+
+const RelayedURL = "ws://localhost:8081";
+const relayerSocket = new WebSocket(RelayedURL);
+
+relayerSocket.onmessage = (event: MessageEvent) => {
+  const data = event.data;
+  const message = typeof data == "string" ? data : data.toString();
+  console.log("Message received: ", message);
+
+  const parsedData = JSON.parse(message);
+  if (parsedData.type == "chat") {
+    const room = parsedData.room;
+    rooms[room]?.sockets.map((socket) => socket.send(data));
+  }
+};
+
+wss.on("connection", function connection(ws: WS) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data: WS.RawData) {
+    const message = typeof data == "string" ? data : data.toString();
+
+    const parsedData = JSON.parse(message);
+    if (parsedData.type == "join-room") {
+      const room = parsedData.room;
+      if (!rooms[room]) {
+        rooms[room] = {
+          sockets: [],
+        };
+      }
+
+      rooms[room].sockets.push(ws);
+    }
+    if (parsedData.type == "chat") {
+      relayerSocket.send(data.toString());
+    }
+  });
+});
